3c7a96390f11ea71d5e68f7cd8e46a3d9600f068
kpk-i710 <47340012+kpk-i710@users.noreply.github.com>
2024-12-04 05:42:25 +0600

    добавил получение книги по id

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import uvicorn
 
 app = FastAPI()
@@ -22,3 +22,10 @@ books = [
 @app.get('/books')
 def read_books():
     return books
+
+@app.get('/books/{book_id}')
+def get_book(book_id:int):
+    for book in books:
+        if book['id'] == book_id:
+            return book
+    raise HTTPException(status_code=404,detail="Книга не найдена")
