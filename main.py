ec6c3edb0a25f8ebb94931504bb4c05d007f59c3
kpk-i710 <47340012+kpk-i710@users.noreply.github.com>
2024-12-04 06:09:58 +0600

    добавил книгу добаление

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from fastapi import FastAPI, HTTPException
 import uvicorn
-from pydantic import  BaseModel
+from pydantic import BaseModel
 
 app = FastAPI()
 
-
-if __name__ == "__main__":
-    uvicorn.run("app.main:app", reload=True)
-
+# if __name__ == "__main__":
+#     uvicorn.run("app.main:app", reload=True)
 
 books = [
     {"id": 1,
@@ -20,17 +18,29 @@ books = [
 ]
 
 
-@app.get('/books',tags=['Книги1'],summary="Все книги")
+@app.get('/books', tags=['Книги1'], summary="Все книги")
 def read_books():
     return books
 
-@app.get('/books/{book_id}',tags=['Книги1'],summary="Получит одну книгу")
-def get_book(book_id:int):
+
+@app.get('/books/{book_id}', tags=['Книги1'], summary="Получит одну книгу")
+def get_book(book_id: int):
     for book in books:
         if book['id'] == book_id:
             return book
-    raise HTTPException(status_code=404,detail="Книга не найдена")
+    raise HTTPException(status_code=404, detail="Книга не найдена")
 
 
-@app.post('books')
-def create_book():
+class NewBook(BaseModel):
+    title: str
+    author: str
+
+
+@app.post('/books',summary='Создать книгу')
+def create_book(new_book: NewBook):
+    books.append({
+        "id": len(books)+1,
+        "title": new_book.title,
+        "author": new_book.author,
+    })
+    return {"success":True,"message": "Книга успешно добавлена"}
